e13ce61e2a213fe9ef59e2b9e487cf089bdd92a0
agussdiaz28 <agussdiaz28@github.com>
2017-05-26 20:56:42 -0300

    ajax fix

--- js/actions.js
+++ js/actions.js
@@ -1,40 +1,53 @@
-$(document).ready(function(){
+$( document ).ready( function() {
 
-    function cargar(result){
-      $("#main").html(result);
-    }
+	function cargar( result ) {
+		$( "#main" ).html( result );
+	};
 
-    function loadajax("page"){
-      let result = page.concat(".html");
-        $.ajax({
-          url: result,
-          dataType: "html"
-          success: cargar
-          }
-        $("li").removeClass("active");
-        $(page).addClass("active");
-    });
+	$( "#home" ).ready( function() {
+		$.ajax( {
+			dataType: "html",
+			url: "home.html",
+			success: cargar
+		} );
+		//$( "li" ).removeClass( "active" );
+		//$( "#home" ).addClass( "active" );
 
-    $("#home").on("click",function(){
-      loadajax("home");
-      /*$.ajax({
-              dataType: "html",
-              url: "home.html",
-              success: cargar
-            });
-            $("li").removeClass("active");
-            $("#home").addClass("active"); */
-    });
+	} );
 
-    $("#experiencias").on("click",function(){
-      loadajax("experiencias");
-    });
+	function loadajax( pagina ) {
+		let result = pagina.concat( ".html" );
+		console.log( result );
+		$.ajax( {
+			dataType: "html",
+			url: result,
+			success: cargar
+		} );
+		$( "li" ).removeClass( "active" );
+		$( pagina ).addClass( "active" );
+	};
 
-    $("#paquetes").on("click",function(){
-      loadajax("paquetes");
-    });
+	$( "#home" ).on( "click", function() {
+		loadajax( "home" );
+		/*$.ajax( {
+			dataType: "html",
+			url: "home.html",
+			success: cargar
+		} );
+		$( "li" ).removeClass( "active" );
+		$( "#home" ).addClass( "active" );*/
+	} );
 
-    $("#contacto").on("click",function(){
-      loadajax("contacto");
-    });
-});
+	$( "#experiencias" ).on( "click", function() {
+		loadajax( "experiencias" );
+	} );
+
+
+	$( "#paquetes" ).on( "click", function() {
+		loadajax( "paquetes" );
+	} );
+
+	$( "#contacto" ).on( "click", function() {
+		loadajax( "contact" );
+	} );
+} );
