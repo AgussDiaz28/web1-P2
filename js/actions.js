ac601ba49484a99f2c7133510931443f41f600aa
AgussDiaz28 <agussdiaz28@gmail.com>
2017-05-31 22:12:50 -0300

    modificaciones

--- js/actions.js
+++ js/actions.js
@@ -1,8 +1,12 @@
 $( document ).ready( function() {
 
-	function cargar( result ) {
-		$( "#main" ).html( result );
-	};
+	numGrupo =  91;
+
+	$("li").ready(function (){
+		$("#main").html("Cargando")
+	});
+
+	"use strict";
 
 	$( "#home" ).ready( function() {
 		$.ajax( {
@@ -10,32 +14,74 @@ $( document ).ready( function() {
 			url: "home.html",
 			success: cargar
 		} );
-		//$( "li" ).removeClass( "active" );
-		//$( "#home" ).addClass( "active" );
+		$( "li" ).removeClass( "active" );
+		$( "#home" ).addClass( "active" );
 
 	} );
 
+	function cargar( result ) {
+		$( "#main" ).html( result );
+	};
+
+	function traerDatosTabla() {
+		$.ajax( {
+			"url": "http://10.10.12.28:5000/api/thing/group/" + numGrupo,
+			"method": "GET",
+			"dataType": "JSON",
+			"success": mostrarDatosTabla,
+			"error": ErrorLog
+		} );
+		$( "..js-table-add" ).html( "<h4> Cargando... </h4>" )
+	}
+
+	function mostrarDatosTabla( data ) {
+		for ( let i = 0; i < data.information.length; i++ ) {
+			html += "<tr>" + "<td>" + data.information[ i ].pais + "</td>" + "<tr>";
+			html += "<tr>" + "<td>" + data.information[ i ].ciudad + "</td>" + "<tr>";
+			html += "<tr>" + "<td>" + data.information[ i ].precioI + "</td>" + "<tr>";
+			html += "<tr>" + "<td>" + data.information[ i ].precioP + "</td>" + "<tr>";
+			html += "<tr>"  + "<td><span class=" + "glyphicon glyphicon-trash" + "></span></td>" + "</tr>" ;
+		}
+		$( ".js-table-add" ).html( html );
+	}
+
+	$( "#guardar" ).on( "click", function( event ) {
+		event.preventDefault();
+		let pais = $( "#pais" ).val();
+		let ciudad = $( "#ciudad" ).val();
+		let precioI = $( "#precioI" ).val();
+		let precioP = $( "#precioP" ).val();
+		let objeto = {
+			"group": numGrupo,
+			"pais": info,
+			"ciudad": ciudad ,
+			"precioI": precioI,
+			"precioP": precioP,
+		}
+		$.ajax( {
+			"url": "http://10.10.12.28:5000/api/thing" + numGrupo,
+			"method": "POST",
+			"contentType": "Application/json; charset=utf-8",
+			"data": JSON.stringify( objeto ),
+			"dataType": "JSON",
+			"success": Guardado ,
+			"error": ErrorLog
+		} )
+	} )
+
 	function loadajax( pagina ) {
 		let result = pagina.concat( ".html" );
-		console.log( result );
 		$.ajax( {
 			dataType: "html",
 			url: result,
 			success: cargar
 		} );
 		$( "li" ).removeClass( "active" );
-		$( pagina ).addClass( "active" );
+		$( "#" + pagina ).addClass( "active" );
 	};
 
 	$( "#home" ).on( "click", function() {
 		loadajax( "home" );
-		/*$.ajax( {
-			dataType: "html",
-			url: "home.html",
-			success: cargar
-		} );
-		$( "li" ).removeClass( "active" );
-		$( "#home" ).addClass( "active" );*/
 	} );
 
 	$( "#experiencias" ).on( "click", function() {
@@ -48,6 +94,11 @@ $( document ).ready( function() {
 	} );
 
 	$( "#contacto" ).on( "click", function() {
-		loadajax( "contact" );
+		loadajax( "contacto" );
 	} );
+
+	$( "#cargado" ).on( "click", function() {
+		loadajax( "cargado" );
+	} );
+
 } );
